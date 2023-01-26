361cf970575e33884e2aa485c995282942ef28bb
Unicode <69970783+Unicode01@users.noreply.github.com>
2023-01-26 16:01:31 +0800

    Add files via upload

--- src/main/java/io/fishermen/fpsdisplay/settings/Dada.java
+++ src/main/java/io/fishermen/fpsdisplay/settings/Dada.java
@@ -4,27 +4,70 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.gui.GuiChat;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.network.play.client.C0DPacketCloseWindow;
+import net.minecraft.network.play.client.C0EPacketClickWindow;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
+import pw.cinque.CommandSettings.CommandSettings;
+import pw.cinque.event.Connection;
+
+import java.util.ArrayList;
 
 public class Dada extends GuiSettings
 {
+    private static CommandSettings M;
+    private static boolean isChoosing;
+    private static boolean ena;
+    private static ArrayList<Object> Packets = new ArrayList<Object>();
     /*
     protected Random r;
     */
 
     public Dada() {
         super(GuiSettings.a(new char[] { 'I', 'n', 'v', 'm', 'o', 'v', 'e' }), "", c4.Player, 0, -1);
+        this.M = new CommandSettings("Mode",1,1,2,1);
+        this.avav(M);
         /*
         this.r = new Random();
          */
+    }
+    @Override
+    public void en(){
+        ena=true;
+    }
+    @Override
+    public void dd(){
+        ena=false;
+    }
+    public static boolean onPacket(Object object, Connection.Side connection_side) {
+        if(!ena || M.g3tV4l4u3() != 2) return true;
+        if(connection_side == Connection.Side.OUT && object instanceof C0EPacketClickWindow && !isChoosing){
+            isChoosing = true;
+            return true;
+        }
+        if(connection_side == Connection.Side.OUT && object instanceof C0DPacketCloseWindow && isChoosing){
+            Packets.add(object);
+            isChoosing = false;
+            return false;
+        }
+        return true;
     }
 
     @SubscribeEvent
     public void a(final PlayerTickEvent a) {
         if(!GuiSettings.isPlayerInGame()){
+            Packets.clear();
             return;
+        }
+        if(!isChoosing && Packets != null){
+            for (Object P : Packets){
+                mc.getNetHandler().addToSendQueue((Packet) P);
+            }
+            Packets.clear();
         }
         final Minecraft mc = Minecraft.getMinecraft();
         if (mc.currentScreen != null) {
